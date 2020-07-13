f9e562d825cad3fded5f53c95bd3d7c74bf6a1b4
tossia <tossia2000@yahoo.fr>
2020-07-13 21:11:58 +0200

    push to github

---
+++ src/test/Game.java
@@ -0,0 +1,43 @@
+package test;
+
+public class Game {
+	private int rolls[] = new int[21];
+	private int currentRoll = 0;
+	
+	public void roll(int pins) {
+		rolls[currentRoll++] = pins;
+	}
+	
+	public int score() {
+		int score = 0;
+		int frameIndex = 0;
+		for (int frame = 0; frame < 10; frame++) {
+			if (rolls[frameIndex] == 10 ) {
+				score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+				frameIndex++;
+			} else if (isSpare(frameIndex)) 			{
+				score += 10 + rolls[frameIndex + 2];
+				frameIndex += 2;
+			} else {
+			score += rolls[frameIndex] + rolls[frameIndex +1];
+			frameIndex += 2;
+			}
+		}
+		return score;
+	}
+	private boolean isStike(int frameIndex) {
+		return rolls[frameIndex] == 10;
+	}
+	private int sumOfBallsInFrame(int frameIndex) {
+		return rolls[frameIndex] + rolls[frameIndex];
+	}
+	private int spareBonus(int frameIndex) {
+		return rolls[frameIndex + 2];
+	}
+	private int strikeBonus(int frameIndex) {
+		return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+	}
+	private boolean isSpare(int frameIndex) {
+		return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+	}
+}
